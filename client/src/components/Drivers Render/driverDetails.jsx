92d1359b823a3364cd11d3fb3b105f758914a00d
Ivan <serralta.ivan@gmail.com>
2023-10-31 20:34:04 -0300

    DriverDetails created. Ref: #6

---
+++ client/src/components/Drivers Render/driverDetails.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function DriverDetails(aDriver){
+    return(
+        <div>
+            <h2>{aDriver.name+' '+aDriver.lastname}</h2>
+            <img src={aDriver.image} alt="" className='cardimg'/>
+            <h4>|{aDriver.Teams?.map(x=>{return x.name + ' |'})
+            }</h4>
+        </div>
+    )
+}
